7e4a5631d4e6180493f54b8ea6bf2556658ce146
Shinpei Aoki <shi.aoki@opt.ne.jp>
2018-12-01 22:15:54 +0900

    ネットワークまで実装

---
+++ gateway.tf
@@ -0,0 +1,15 @@
+#########################
+# Gateway
+#########################
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "main-gw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name    = "${var.product}-${terraform.workspace}-main-gw"
+    Product = "${var.product}"
+    Env     = "${terraform.workspace}"
+  }
+}
