82ed3faf843e52d3d54729e14c0fa866ebbac64c
Shinpei Aoki <shi.aoki@opt.ne.jp>
2020-06-06 23:32:22 +0900

    pushスクリプトの修正

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -7,6 +7,6 @@ resource "aws_ecr_repository" "repository-api" {
   name = "${var.product}-api"
 
   provisioner "local-exec" {
-    command = "./push-image.sh ${var.profile} api ${aws_ecr_repository.repository-api.name} ${aws_ecr_repository.repository-api.repository_url}"
+    command = "../scripts/push-image.sh ${var.profile} ../api ${aws_ecr_repository.repository-api.name} ${aws_ecr_repository.repository-api.repository_url}"
   }
 }
