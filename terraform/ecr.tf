1c21bec8260d420b5933b2c5824e38ddbeee6ab1
Shinpei Aoki <shi.aoki@opt.ne.jp>
2020-06-06 23:36:20 +0900

    自動スキャンを追加

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,6 +6,9 @@
 resource "aws_ecr_repository" "repository-api" {
   name = "${var.product}-api"
 
+  image_scanning_configuration {
+    scan_on_push = true
+  }
   provisioner "local-exec" {
     command = "../scripts/push-image.sh ${var.profile} ../api ${aws_ecr_repository.repository-api.name} ${aws_ecr_repository.repository-api.repository_url}"
   }
