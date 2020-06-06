89a7de2b4ed68175d936fc6ed69f50dc5e104b1e
Shinpei Aoki <shi.aoki@opt.ne.jp>
2020-06-06 21:04:26 +0900

    DBは最小構成としては不要なので一旦除外

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,6 @@ variable "region" {}
 variable "profile" {}
 variable "product" {}
 
-variable "db_storage" {}
-variable "db_name" {}
-variable "db_user" {}
-variable "db_pass" {}
-
 variable "front_cpu" {}
 variable "front_memory" {}
 variable "api_cpu" {}
